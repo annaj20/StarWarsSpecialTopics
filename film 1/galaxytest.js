1aa11bb0a6e407d7b35d39974af8171379aa6fd5
annaj20 <37802973+annaj20@users.noreply.github.com>
2018-06-19 20:20:19 +0200

    Update galaxytest.js

--- film 1/galaxytest.js
+++ film 1/galaxytest.js
@@ -485,6 +485,6 @@ class Ship {
     display() {
         imageMode(CENTER);
         fill(this.c) 
-        ellipse(this.pos.x, this.pos.y, 30, 30);
+        ellipse(this.pos.x+this.offset.x, this.pos.y+this.offset.y, 30, 30);
     }
 }
