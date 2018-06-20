3030695a8292a8be5d0db75b1b199f5d7203453a
meyouwe123 <37802965+meyouwe123@users.noreply.github.com>
2018-06-20 12:56:08 +0200

    Update galaxytest.js

--- film 2/galaxytest.js
+++ film 2/galaxytest.js
@@ -353,7 +353,8 @@ function setup() {
         } else if (i == 5){
             c = color(0, 137, 0)
         } else {
-            c = color(255, 255 , 255)
+            for (let place in place)
+                place.color(255, 255, 255)
         }
         ships.push(new Ship(current_ship.journey[0].start[0], current_ship.journey[0].start[1], c, current_ship, i*4, i*4))
     }
