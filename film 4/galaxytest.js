519bb286bce22300e82ca8771df24702d5fa5baf
annaj20 <37802973+annaj20@users.noreply.github.com>
2018-06-20 13:18:34 +0200

    Update galaxytest.js

--- film 4/galaxytest.js
+++ film 4/galaxytest.js
@@ -317,7 +317,7 @@ function setup() {
         current_ship = shipdata[i]
         c = defaultimage
         if (i == 0){
-            c = color(0, 0, 0)
+            c = color(133, 136, 139)
         } else if (i == 1){
             c = color(153, 179, 245)
         } else if (i == 2){
